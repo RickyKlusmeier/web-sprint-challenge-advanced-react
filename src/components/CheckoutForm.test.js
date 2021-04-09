e72b7a8c83436f0ee040e000465bbd1662107502
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro-2.local>
2021-04-09 09:07:25 -0600

    WIP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -4,6 +4,33 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", async () => {
+    render(<CheckoutForm/>)
+    const header = await screen.getByText('Checkout Form')
+    expect(header).toBeVisible
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>)
+
+    const firstName = screen.getByLabelText("First Name:")
+    const lastName = screen.getByLabelText("Last Name:")
+    const address = screen.getByLabelText("Address:")
+    const city = screen.getByLabelText("City:")
+    const state = screen.getByLabelText("State:")
+    const zip = screen.getByLabelText("Zip:")
+    const checkoutButton = screen.getByRole("button")
+
+    userEvent.type(firstName, "Ricky")
+    userEvent.type(lastName, "Klus")
+    userEvent.type(address, "123 Amazon")
+    userEvent.type(city, "Broomfield")
+    userEvent.type(state, "CO")
+    userEvent.type(zip, "80023")
+    userEvent.click(checkoutButton)
+
+    const successMessage = await screen.getByText(/You have ordered some plants! Woo-hoo!/i)
+    expect(successMessage).toBeVisible
+
+
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,30 @@
 // write your custom hook here to control your checkout form
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialValue = {
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: ''
+};
+
+const useForm = () => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue)
+
+    const handleChanges = (evt) => {
+        setValues({...values, [evt.target.name]: evt.target.value})
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+
+        showSuccessMessage(true)
+    }
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
+}
+export default useForm
